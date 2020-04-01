e9c88d692be456a190e16bfc5570e76de7e05058
majakuz <majakuz96@gmail.com>
2020-04-01 22:04:00 +0200

    added LoginComponent route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShowAllCertificatesComponent } from './show-all-certificates/show-all-certificates.component';
 import { CreateNewCertificateComponent } from './create-new-certificate/create-new-certificate.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
     { path: 'showAllCertificates', component: ShowAllCertificatesComponent},
-    { path: 'createNewCertificate', component: CreateNewCertificateComponent}
+    { path: 'createNewCertificate', component: CreateNewCertificateComponent},
+    { path: '', pathMatch: 'full',  component: LoginComponent}
 ];
 
 @NgModule({
@@ -14,4 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ShowAllCertificatesComponent, CreateNewCertificateComponent];
+export const routingComponents = [ShowAllCertificatesComponent, CreateNewCertificateComponent, LoginComponent];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule, routingComponents } from "./app-routing.module";
-import { LoginComponent } from './login/login.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
+
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+  declarations: [
+    AppComponent,
+    routingComponents
   ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
