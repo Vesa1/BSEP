3a551dbd6b9e062122c688ef6ce34a3ff1c644b1
majakuz <majakuz96@gmail.com>
2020-04-01 21:38:57 +0200

    merge

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -9,19 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.LoginDataDTO;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
 
+	private final static String USERNAME_ADMIN = "admin";
+	private final static String PASSWORD_ADMIN = "adminAdmin";
+
+	
 	
 	 @RequestMapping(
-	            value = "/test",
+	            value = "/login",
 	            method = RequestMethod.POST,
 				consumes = MediaType.APPLICATION_JSON_VALUE,
 				produces = MediaType.APPLICATION_JSON_VALUE)
-	    public ResponseEntity logout(@RequestBody String body){
-	       	System.out.println("HELLO" + body);
-	        return new ResponseEntity(HttpStatus.OK);
+	    public ResponseEntity logout(@RequestBody LoginDataDTO loginCredentials){
+	       	
+		 	if(loginCredentials != null) {
+		 		String username = loginCredentials.getUsername();
+		 		String password = loginCredentials.getPassword();
+		 		if(!username.equals(USERNAME_ADMIN) && !password.equals(PASSWORD_ADMIN)) {
+			        return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		 		}
+		 	}
+	       	
+	        return new ResponseEntity(loginCredentials, HttpStatus.OK);
 	    }
-	
 }
