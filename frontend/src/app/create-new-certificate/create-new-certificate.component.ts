73a43bf15e11018ff5ed7970295e0fc148e2a668
majakuz <majakuz96@gmail.com>
2020-04-06 20:17:49 +0200

    form validation

--- frontend/src/app/create-new-certificate/create-new-certificate.component.ts
+++ frontend/src/app/create-new-certificate/create-new-certificate.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { NewCertificate} from '../model/new-certificate';
-import { CreateNewCertificateService} from '../services/create-new-certificate.service';
+import { NewCertificate } from '../model/new-certificate';
+import { CreateNewCertificateService } from '../services/create-new-certificate.service';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-create-new-certificate',
   templateUrl: './create-new-certificate.component.html',
   styleUrls: ['./create-new-certificate.component.css']
 })
 export class CreateNewCertificateComponent implements OnInit {
-
-  newCertificate : NewCertificate;
+  isClicked = false;
+  newCertificate: NewCertificate;
   private selfSigned;
 
-  constructor(private createNewCertificateService: CreateNewCertificateService) { }
+  constructor(
+    private createNewCertificateService: CreateNewCertificateService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSumbitCreateNew(newCertificateForm) {
-    this.newCertificate = newCertificateForm.value;
-    console.log(this.newCertificate);
-    this.createNewCertificateService.createNewCertificate(this.newCertificate);
+  onSumbitCreateNew(newCertificateForm: NgForm) {
+    this.isClicked = true;
+    if (newCertificateForm.form.valid) {
+      this.newCertificate = newCertificateForm.value;
+      this.createNewCertificateService.createNewCertificate(
+        this.newCertificate
+      );
+    }
   }
 }
